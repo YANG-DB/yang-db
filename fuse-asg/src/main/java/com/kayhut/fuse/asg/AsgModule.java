50e91f6418c0d344e89e573f9207f3a1aa1dfbd5
lior <lior.perry@kayhut.com>
2017-02-22 09:39:34 +0200

    add configuration for dynamic loading of modules

---
+++ fuse-asg/src/main/java/com/kayhut/fuse/asg/AsgModule.java
@@ -0,0 +1,17 @@
+package com.kayhut.fuse.asg;
+
+import com.google.inject.Binder;
+import com.typesafe.config.Config;
+import org.jooby.Env;
+import org.jooby.Jooby;
+
+/**
+ * Created by lior on 22/02/2017.
+ */
+public class AsgModule implements Jooby.Module  {
+
+    @Override
+    public void configure(Env env, Config conf, Binder binder) throws Throwable {
+        binder.bind(AsgDriver.class).to(BaseAsgDriver.class).asEagerSingleton();
+    }
+}
