eee9a559e731d550af0218f0bd49955f257836ce
Roman Margolis <roman.margolis@kayhut.com>
2017-02-23 09:37:45 +0200

    added the plan model

---
+++ fuse-model/src/main/java/com/kayhut/fuse/model/execution/plan/LogicalOperator.java
@@ -0,0 +1,9 @@
+package com.kayhut.fuse.model.execution.plan;
+
+/**
+ * Created by User on 23/02/2017.
+ */
+public enum LogicalOperator {
+    and,
+    or
+}
--- fuse-model/src/main/java/com/kayhut/fuse/model/execution/plan/PlanOpBase.java
+++ fuse-model/src/main/java/com/kayhut/fuse/model/execution/plan/PlanOpBase.java
@@ -3,6 +3,6 @@ package com.kayhut.fuse.model.execution.plan;
 /**
  * Created by User on 20/02/2017.
  */
-public abstract class PlanOp {
+public abstract class PlanOpBase {
 
 }
