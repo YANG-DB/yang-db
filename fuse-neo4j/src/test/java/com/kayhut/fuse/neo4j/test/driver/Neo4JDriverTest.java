818e61172b2c67f5adfd8b0f237ab14dbadf4742
elad <eladw@kayhut.com>
2017-02-23 13:25:37 +0200

    Adding neo4j module

--- fuse-neo4j/src/test/java/com/kayhut/fuse/neo4j/test/driver/Neo4JDriverTest.java
+++ fuse-neo4j/src/test/java/com/kayhut/fuse/neo4j/test/driver/Neo4JDriverTest.java
@@ -1,12 +1,6 @@
 package com.kayhut.fuse.neo4j.test.driver;
 
-import com.kayhut.fuse.neo4j.driver.Neo4jDriver;
-
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.Rule;
-
-import static org.junit.Assert.assertTrue;
 
 
 /**
@@ -14,16 +8,11 @@ import static org.junit.Assert.assertTrue;
  */
 public class Neo4JDriverTest {
 
-    @Ignore
     @Test
-    @Ignore
     public void checkDriverConnection() {
 
-        Neo4jDriver driver = new Neo4jDriver();
 
-        String result = driver.query("match (p:PERSON) return p");
 
-        assertTrue(result != null);
     }
 
 }
