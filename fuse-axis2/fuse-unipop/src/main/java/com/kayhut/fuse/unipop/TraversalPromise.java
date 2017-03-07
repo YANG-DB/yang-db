befeae898568dbad383c1d94061be8e37b347ac5
Roman Margolis <roman.margolis@kayhut.com>
2017-03-07 13:23:02 +0200

    1. Created the Promise, IdPromise and TraversalPromise classes and interfaces 2. Created the Constraint and TraversalConstraint classes and interfaces

---
+++ fuse-axis2/fuse-unipop/src/main/java/com/kayhut/fuse/unipop/TraversalPromise.java
@@ -0,0 +1,36 @@
+package com.kayhut.fuse.unipop;
+
+import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
+
+/**
+ * Created by User on 07/03/2017.
+ */
+public class TraversalPromise implements Promise{
+    //region Constructor
+    public TraversalPromise(Object id, Traversal traversal) {
+        this.id = id;
+        this.traversal = traversal;
+    }
+    //endregion
+
+    //region Promise Implementation
+    public Object getId() {
+        return id;
+    }
+    //endregion
+
+    //region properties
+
+    //endregion
+
+    @Override
+    public String toString() {
+        return getId().toString() + ": " + traversal.toString();
+    }
+
+
+    //region fields
+    private Object id;
+    private Traversal traversal;
+    //endregion
+}
