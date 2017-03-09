633c442d119921da946265e98ea8a1c763f71df7
Roman Margolis <roman.margolis@kayhut.com>
2017-03-09 18:45:34 +0200

    store resource info
    1. added handling for store resources
    2. added a proper PageResource object

---
+++ fuse-dispatcher/src/main/java/com/kayhut/fuse/dispatcher/resource/PageResource.java
@@ -0,0 +1,7 @@
+package com.kayhut.fuse.dispatcher.resource;
+
+/**
+ * Created by User on 09/03/2017.
+ */
+public class PageResource {
+}
---
+++ fuse-model/src/main/java/com/kayhut/fuse/model/resourceInfo/StoreResourceInfo.java
@@ -0,0 +1,7 @@
+package com.kayhut.fuse.model.resourceInfo;
+
+/**
+ * Created by User on 09/03/2017.
+ */
+public class StoreResourceInfo {
+}
