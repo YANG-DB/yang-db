7f7651eb8cb101ddf206570ef7f0f444c9b5578c
Moti Cohen <mordechaic@kayhut.com>
2017-03-15 22:33:39 +0200

    epb module guice definitions

--- fuse-axis2/fuse-epb/src/main/java/com/kayhut/fuse/epb/EpbModule.java
+++ fuse-axis2/fuse-epb/src/main/java/com/kayhut/fuse/epb/EpbModule.java
@@ -1,6 +1,17 @@
 package com.kayhut.fuse.epb;
 
 import com.google.inject.Binder;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Names;
+import com.kayhut.fuse.epb.plan.*;
+import com.kayhut.fuse.epb.plan.extenders.AllDirectionsPlanExtensionStrategy;
+import com.kayhut.fuse.epb.plan.extenders.CompositePlanExtensionStrategy;
+import com.kayhut.fuse.epb.plan.extenders.InitialPlanGeneratorExtensionStrategy;
+import com.kayhut.fuse.epb.plan.validation.SiblingOnlyPlanValidator;
+import com.kayhut.fuse.epb.plan.wrappers.SimpleWrapperFactory;
+import com.kayhut.fuse.model.asgQuery.AsgQuery;
+import com.kayhut.fuse.model.execution.plan.Plan;
+import com.kayhut.fuse.model.execution.plan.costs.SingleCost;
 import com.typesafe.config.Config;
 import org.jooby.Env;
 import org.jooby.Jooby;
@@ -13,6 +24,12 @@ public class EpbModule implements Jooby.Module  {
     @Override
     public void configure(Env env, Config conf, Binder binder) throws Throwable {
         //binder.bind(EpbDriver.class).to(BaseEpbDriver.class).asEagerSingleton();
+        CompositePlanExtensionStrategy<Plan, AsgQuery> compositePlanExtensionStrategy = new CompositePlanExtensionStrategy<>(new InitialPlanGeneratorExtensionStrategy(), new AllDirectionsPlanExtensionStrategy());
+        binder.bind(new TypeLiteral<PlanExtensionStrategy<Plan, AsgQuery>>(){}).toInstance(compositePlanExtensionStrategy);
+        binder.bind(new TypeLiteral<PlanPruneStrategy<Plan, SingleCost>>(){}).annotatedWith(Names.named("GlobalPruningStrategy")).toInstance(new NoPruningPruneStrategy<>());
+        binder.bind(new TypeLiteral<PlanPruneStrategy<Plan, SingleCost>>(){}).annotatedWith(Names.named("LocalPruningStrategy")).toInstance(new NoPruningPruneStrategy<>());
+        binder.bind(new TypeLiteral<PlanValidator<Plan, AsgQuery>>(){}).toInstance(new SiblingOnlyPlanValidator());
+        binder.bind(new TypeLiteral<PlanWrapperFactory<Plan, AsgQuery, SingleCost>>(){}).toInstance(new SimpleWrapperFactory());
 
     }
 }
