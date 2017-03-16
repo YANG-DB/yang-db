7e4613efab3f0248862b82d95dcfd0a1d6c898d8
moti cohen <mordechaic@kayhut.com>
2017-03-16 13:31:30 +0200

    Partial implementation of elastic populator

--- fuse-test/fuse-test-framework/src/main/java/com/kayhut/test/framework/scenario/DataScenarioDescriptor.java
+++ fuse-test/fuse-test-framework/src/main/java/com/kayhut/test/framework/scenario/DataScenarioDescriptor.java
@@ -1,7 +1,11 @@
 package com.kayhut.test.framework.scenario;
 
+import java.util.stream.Stream;
+
 /**
  * Created by moti on 3/12/2017.
  */
 public interface DataScenarioDescriptor {
+
+    Stream<ScenarioDocument> getScenarioDocuments();
 }
--- fuse-test/fuse-test-framework/src/main/java/com/kayhut/test/framework/scenario/FolderScenarioDescriptor.java
+++ fuse-test/fuse-test-framework/src/main/java/com/kayhut/test/framework/scenario/FolderScenarioDescriptor.java
@@ -1,7 +1,13 @@
 package com.kayhut.test.framework.scenario;
 
+import java.util.stream.Stream;
+
 /**
  * Created by moti on 3/12/2017.
  */
 public class FolderScenarioDescriptor implements DataScenarioDescriptor {
+    @Override
+    public Stream<ScenarioDocument> getScenarioDocuments() {
+        return null;
+    }
 }
