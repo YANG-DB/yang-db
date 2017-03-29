fcbd6e6850ab31e9a029f2d25637d94b0617bdf4
shimonbi <shimonbi@kayhut.com>
2017-03-29 14:03:03 +0300

    Adding tests to IndexSearchAppender

--- fuse-axis2/fuse-unipop/src/main/java/com/kayhut/fuse/unipop/controller/search/appender/IndexSearchAppender.java
+++ fuse-axis2/fuse-unipop/src/main/java/com/kayhut/fuse/unipop/controller/search/appender/IndexSearchAppender.java
@@ -5,7 +5,6 @@ import com.google.common.collect.Streams;
 import com.kayhut.fuse.unipop.controller.search.SearchBuilder;
 import com.kayhut.fuse.unipop.controller.context.PromiseElementControllerContext;
 import com.kayhut.fuse.unipop.controller.utils.TraversalValuesByKeyProvider;
-import com.kayhut.fuse.unipop.promise.Constraint;
 import com.kayhut.fuse.unipop.schemaProviders.*;
 import com.kayhut.fuse.unipop.schemaProviders.indexPartitions.IndexPartition;
 import com.kayhut.fuse.unipop.structure.ElementType;
@@ -23,54 +22,51 @@ public class IndexSearchAppender implements SearchAppender<PromiseElementControl
     //region SearchAppender Implementation
     @Override
     public boolean append(SearchBuilder searchBuilder, PromiseElementControllerContext promiseElementControllerContext) {
-        try {
-            GraphElementSchemaProvider schemaProvider = promiseElementControllerContext.getSchemaProvider();
-            Traversal traversal = promiseElementControllerContext.getConstraint().get().getTraversal();
-            TraversalValuesByKeyProvider traversalValuesByKeyProvider = new TraversalValuesByKeyProvider();
-            Set<String> labels = traversalValuesByKeyProvider.getValueByKey(traversal, "label");
+        GraphElementSchemaProvider schemaProvider = promiseElementControllerContext.getSchemaProvider();
+        Traversal traversal = promiseElementControllerContext.getConstraint().get().getTraversal();
+        TraversalValuesByKeyProvider traversalValuesByKeyProvider = new TraversalValuesByKeyProvider();
+        Set<String> labels = traversalValuesByKeyProvider.getValueByKey(traversal, "label");
 
-            if (!labels.isEmpty()) {
-                labels.stream().forEach(label -> {
-                    if (promiseElementControllerContext.getElementType() == ElementType.edge) {
-                        Optional<Iterable<GraphEdgeSchema>> edgeSchemas = schemaProvider.getEdgeSchemas(label);
-                        if (edgeSchemas.isPresent()) {
-                            searchBuilder.getIndices().addAll(getEdgeSchemasIndices(edgeSchemas.get()));
-                        }
+        if (!labels.isEmpty()) {
+            labels.stream().forEach(label -> {
+                if (promiseElementControllerContext.getElementType() == ElementType.edge) {
+                    Optional<Iterable<GraphEdgeSchema>> edgeSchemas = schemaProvider.getEdgeSchemas(label);
+                    if (edgeSchemas.isPresent()) {
+                        searchBuilder.getIndices().addAll(getEdgeSchemasIndices(edgeSchemas.get()));
                     }
-                    else if (promiseElementControllerContext.getElementType() == ElementType.vertex)
-                    {
-                        Optional<GraphVertexSchema> vertexSchema = schemaProvider.getVertexSchema(label);
-                        if(vertexSchema.isPresent()) {
-                            searchBuilder.getIndices().addAll(getVertexSchemasIndices(vertexSchema));
-                        }
+                }
+                else if (promiseElementControllerContext.getElementType() == ElementType.vertex)
+                {
+                    Optional<GraphVertexSchema> vertexSchema = schemaProvider.getVertexSchema(label);
+                    if(vertexSchema.isPresent()) {
+                        searchBuilder.getIndices().addAll(getVertexSchemasIndices(vertexSchema));
+                    }
+                }
+            });
+            return true;
+        }
+        else // No specific label - append all index partitions filtered by the type of the element (vertex or edge)
+        {
+            if (promiseElementControllerContext.getElementType() == ElementType.vertex) {
+                Iterable<String> vertexTypes = schemaProvider.getVertexTypes();
+                vertexTypes.forEach(vertexType -> {
+                    Optional<GraphVertexSchema> vertexSchema = schemaProvider.getVertexSchema(vertexType);
+                    if(vertexSchema.isPresent()) {
+                        searchBuilder.getIndices().addAll(getVertexSchemasIndices(vertexSchema));
                     }
                 });
-                return true;
             }
-            else // No specific label - search in all index partitions filtered by the type of the element (vertex or edge)
-            {
-                if (promiseElementControllerContext.getElementType() == ElementType.vertex) {
-                    Iterable<String> vertexTypes = schemaProvider.getVertexTypes();
-                    vertexTypes.forEach(vertexType -> {
-                        Optional<GraphVertexSchema> vertexSchema = schemaProvider.getVertexSchema(vertexType);
-                        if(vertexSchema.isPresent()) {
-                            searchBuilder.getIndices().addAll(getVertexSchemasIndices(vertexSchema));
-                        }
-                    });
-                }
-                else if (promiseElementControllerContext.getElementType() == ElementType.edge) {
-                    Iterable<String> edgeTypes = schemaProvider.getEdgeTypes();
-                    edgeTypes.forEach(edgeType -> {
-                        Optional<Iterable<GraphEdgeSchema>> edgeSchemas = schemaProvider.getEdgeSchemas(edgeType);
-                        if (edgeSchemas.isPresent()){
-                            searchBuilder.getIndices().addAll(getEdgeSchemasIndices(edgeSchemas.get()));
-                        }
-                    });
-                }
+            else if (promiseElementControllerContext.getElementType() == ElementType.edge) {
+                Iterable<String> edgeTypes = schemaProvider.getEdgeTypes();
+                edgeTypes.forEach(edgeType -> {
+                    Optional<Iterable<GraphEdgeSchema>> edgeSchemas = schemaProvider.getEdgeSchemas(edgeType);
+                    if (edgeSchemas.isPresent()){
+                        searchBuilder.getIndices().addAll(getEdgeSchemasIndices(edgeSchemas.get()));
+                    }
+                });
             }
-        } catch (Exception e) {
-            return false;
         }
+
         return true;
     }
 
--- fuse-axis2/fuse-unipop/src/test/java/com/kayhut/fuse/unipop/controller/search/appender/IndexSearchAppenderTest.java
+++ fuse-axis2/fuse-unipop/src/test/java/com/kayhut/fuse/unipop/controller/search/appender/IndexSearchAppenderTest.java
@@ -1,16 +1,139 @@
 package com.kayhut.fuse.unipop.controller.search.appender;
 
+import com.google.common.collect.Iterators;
+import com.kayhut.fuse.model.ontology.EPair;
+import com.kayhut.fuse.model.ontology.EntityType;
+import com.kayhut.fuse.model.ontology.Ontology;
+import com.kayhut.fuse.model.ontology.RelationshipType;
+import com.kayhut.fuse.unipop.controller.context.PromiseElementControllerContext;
+import com.kayhut.fuse.unipop.controller.search.SearchBuilder;
+import com.kayhut.fuse.unipop.promise.Promise;
+import com.kayhut.fuse.unipop.promise.TraversalConstraint;
+import com.kayhut.fuse.unipop.schemaProviders.GraphElementSchemaProvider;
+import com.kayhut.fuse.unipop.schemaProviders.OntologySchemaProvider;
+import com.kayhut.fuse.unipop.structure.ElementType;
+import javaslang.collection.Stream;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.*;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by benishue on 29-Mar-17.
  */
 public class IndexSearchAppenderTest {
+
     @Test
-    public void append() throws Exception {
-        assertEquals(1,1);
+    public void appendTest_Constraint_labelDragon() throws Exception {
+        Ontology ontology = getOntology();
+        GraphElementSchemaProvider schemaProvider = getOntologySchemaProvider(ontology);
+        TraversalConstraint traversalConstraint = new TraversalConstraint(__.has("label","Dragon"));
+        PromiseElementControllerContext promiseElementControllerContext = new
+                PromiseElementControllerContext(new ArrayList<>(), Optional.of(traversalConstraint),schemaProvider,ElementType.vertex);
+
+        SearchBuilder searchBuilder = new SearchBuilder();
+        IndexSearchAppender indexSearchAppender = new IndexSearchAppender();
+        boolean appendResult = indexSearchAppender.append(searchBuilder,promiseElementControllerContext);
+
+        assertEquals(appendResult, true);
+        assertEquals(2,searchBuilder.getIndices().size());
+        assertTrue(searchBuilder.getIndices().contains("dragonIndex1"));
+        assertTrue(searchBuilder.getIndices().contains("dragonIndex2"));
+        assertFalse(searchBuilder.getIndices().contains("vertexIndex1"));
+    }
+
+    @Test
+    public void appendTest_Constraint_labelPerson() throws Exception {
+        Ontology ontology = getOntology();
+        GraphElementSchemaProvider schemaProvider = getOntologySchemaProvider(ontology);
+        TraversalConstraint traversalConstraint = new TraversalConstraint(__.has("label","Person"));
+        PromiseElementControllerContext promiseElementControllerContext = new
+                PromiseElementControllerContext(new ArrayList<>(), Optional.of(traversalConstraint),schemaProvider,ElementType.vertex);
+
+        SearchBuilder searchBuilder = new SearchBuilder();
+        IndexSearchAppender indexSearchAppender = new IndexSearchAppender();
+        boolean appendResult = indexSearchAppender.append(searchBuilder,promiseElementControllerContext);
+
+        assertEquals(appendResult, true);
+        assertEquals(1,searchBuilder.getIndices().size());
+        assertTrue(searchBuilder.getIndices().contains("personIndex1"));
+        assertFalse(searchBuilder.getIndices().contains("vertexIndex1"));
+    }
+
+
+    @Test
+    public void appendTest_Constraint_noLabel() throws Exception {
+        //This test should return all the indices of the schema
+        Ontology ontology = getOntology();
+        GraphElementSchemaProvider schemaProvider = getOntologySchemaProvider(ontology);
+        TraversalConstraint traversalConstraint = new TraversalConstraint(__.has("color","sheker"));
+        PromiseElementControllerContext promiseElementControllerContext = new
+                PromiseElementControllerContext(new ArrayList<>(), Optional.of(traversalConstraint),schemaProvider,ElementType.vertex);
+
+        SearchBuilder searchBuilder = new SearchBuilder();
+        IndexSearchAppender indexSearchAppender = new IndexSearchAppender();
+        boolean appendResult = indexSearchAppender.append(searchBuilder,promiseElementControllerContext);
+
+        assertEquals(appendResult, true);
+        assertEquals(3,searchBuilder.getIndices().size());
+        assertTrue(searchBuilder.getIndices().contains("personIndex1"));
+        assertFalse(searchBuilder.getIndices().contains("vertexIndex1"));
+    }
+
+    //region Private Methods
+    private OntologySchemaProvider getOntologySchemaProvider(Ontology ontology) {
+        return new OntologySchemaProvider((label, elementType) -> {
+            if (elementType == ElementType.vertex) {
+                if (label.equals("Dragon")){
+                    return Collections.singletonList(() -> Arrays.asList("dragonIndex1", "dragonIndex2"));
+                }
+                else if(label.equals("Person")){
+                    return Collections.singletonList(() -> Arrays.asList("personIndex1"));
+                }
+                //Default
+                return Collections.singletonList(() -> Arrays.asList("vertexIndex1", "vertexIndex2"));
+            } else if (elementType == ElementType.edge) {
+                return Collections.singletonList(() -> Arrays.asList("edgeIndex1", "edgeIndex2"));
+            } else {
+                // must fail
+                Assert.assertTrue(false);
+                return null;
+            }
+        }, ontology);
+    }
+
+    private Ontology getOntology() {
+        Ontology ontology = Mockito.mock(Ontology.class);
+        List<EPair> ePairs = Arrays.asList(new EPair() {{
+            seteTypeA(2);
+            seteTypeB(1);
+        }});
+        RelationshipType fireRelationshipType = RelationshipType.RelationshipTypeBuilder.aRelationshipType()
+                .withRType(1).withName("Fire").withEPairs(ePairs).build();
+        when(ontology.getEntityTypes()).thenAnswer(invocationOnMock ->
+                {
+                    ArrayList<EntityType> entityTypes = new ArrayList<>();
+                    entityTypes.add(EntityType.EntityTypeBuilder.anEntityType()
+                            .withEType(1).withName("Person").build());
+                    entityTypes.add(EntityType.EntityTypeBuilder.anEntityType()
+                            .withEType(2).withName("Dragon").build());
+                    return entityTypes;
+                }
+        );
+        when(ontology.getRelationshipTypes()).thenAnswer(invocationOnMock ->
+                {
+                    ArrayList<RelationshipType> relTypes = new ArrayList<>();
+                    relTypes.add(fireRelationshipType);
+                    return relTypes;
+                }
+        );
+
+        return ontology;
     }
 
 }
