6eda0b309d0d4ccc972033dc71f7d378f40c59f7
eladw <eladw@kayhut.com>
2017-04-27 19:01:19 +0300

    adding SearchPromiseVertexFilterController

---
+++ fuse-axis2/fuse-unipop/src/main/java/com/kayhut/fuse/unipop/controller/utils/SearchPromiseVertexFilterController.java
@@ -0,0 +1,99 @@
+package com.kayhut.fuse.unipop.controller.utils;
+
+import com.kayhut.fuse.unipop.controller.ElasticGraphConfiguration;
+import com.kayhut.fuse.unipop.controller.GlobalConstants;
+import com.kayhut.fuse.unipop.controller.search.QueryBuilder;
+import com.kayhut.fuse.unipop.controller.search.SearchBuilder;
+import com.kayhut.fuse.unipop.promise.TraversalConstraint;
+import com.kayhut.fuse.unipop.schemaProviders.GraphElementSchemaProvider;
+import javaslang.collection.Stream;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.HasContainer;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.Client;
+import org.unipop.query.search.SearchVertexQuery;
+import org.unipop.structure.UniGraph;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by Elad on 4/27/2017.
+ * This controller handles constraints on the destination vertices of promise edges.
+ * These constraints are modeled as constraints on special virtual 'promise-filter' edges.
+ * The controller starts with promise-vertices, filter these vertices
+ * and build promise-edges containing the result vertices as end vertices.
+ */
+public class SearchPromiseVertexFilterController implements SearchVertexQuery.SearchVertexController {
+
+    //region Constructors
+    public SearchPromiseVertexFilterController(Client client, ElasticGraphConfiguration configuration, UniGraph graph, GraphElementSchemaProvider schemaProvider) {
+        this.client = client;
+        this.configuration = configuration;
+        this.graph = graph;
+        this.schemaProvider = schemaProvider;
+    }
+
+    //endregion
+    @Override
+    public Iterator<Edge> search(SearchVertexQuery searchVertexQuery) {
+
+        if (searchVertexQuery.getVertices().size() == 0){
+            throw new UnsupportedOperationException("SearchVertexQuery must receive a non-empty list of vertices to start with");
+        }
+
+        List<HasContainer> constraintHasContainers = Stream.ofAll(searchVertexQuery.getPredicates().getPredicates())
+                .filter(hasContainer -> hasContainer.getKey()
+                        .toLowerCase()
+                        .equals(GlobalConstants.HasKeys.CONSTRAINT))
+                .toJavaList();
+
+        if (constraintHasContainers.size() > 1){
+            throw new UnsupportedOperationException("Single \"" + GlobalConstants.HasKeys.CONSTRAINT + "\" allowed");
+        }
+
+        Optional<TraversalConstraint> constraint = Optional.empty();
+        if(constraintHasContainers.size() > 0) {
+            constraint = Optional.of((TraversalConstraint) constraintHasContainers.get(0).getValue());
+        }
+
+        return filterPromiseVertices(searchVertexQuery.getVertices(), constraint);
+
+    }
+
+    private Iterator<Edge> filterPromiseVertices(List<Vertex> vertices, Optional<TraversalConstraint> constraint) {
+
+        SearchBuilder searchBuilder = new SearchBuilder();
+
+        QueryBuilder queryBuilder = searchBuilder.getQueryBuilder().seekRoot().query().filtered().filter().bool().must();
+        TraversalQueryTranslator traversalQueryTranslator = new TraversalQueryTranslator(queryBuilder, false);
+        traversalQueryTranslator.visit(constraint.get().getTraversal());
+
+        searchBuilder.setLimit(100);
+        searchBuilder.setScrollSize(100);
+        searchBuilder.setScrollTime(1000);
+
+        //TODO: Understand how to use the scrolling here to get the response!!!
+        SearchRequestBuilder searchRequest = searchBuilder.compose(client, true).setSearchType(SearchType.SCAN);
+
+        SearchResponse response = searchRequest.execute().actionGet();
+
+        return convert(response);
+    }
+
+    private Iterator<Edge> convert(SearchResponse response) {
+        return Collections.emptyIterator();
+    }
+
+    //region Fields
+    private UniGraph graph;
+    private GraphElementSchemaProvider schemaProvider;
+    private Client client;
+    private ElasticGraphConfiguration configuration;
+    //endregion
+}
