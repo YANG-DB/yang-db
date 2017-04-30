d9483ab151feefa5477ae3b3a7a4e2ae946c4d99
shimonbi <shimonbi@kayhut.com>
2017-04-30 10:13:08 +0300

    no message

--- fuse-stat/src/main/java/com/kayhut/fuse/stat/StatisticsCalculator.java
+++ fuse-stat/src/main/java/com/kayhut/fuse/stat/StatisticsCalculator.java
@@ -1,7 +1,12 @@
 package com.kayhut.fuse.stat;
 
+import org.elasticsearch.client.transport.TransportClient;
+
+
 /**
  * Created by benishue on 27-Apr-17.
  */
 public class StatisticsCalculator {
+
+
 }
