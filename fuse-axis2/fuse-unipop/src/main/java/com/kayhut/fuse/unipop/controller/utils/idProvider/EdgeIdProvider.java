658bc79510072ff3ab47876149553f53c73a3c21
Roman Margolis <roman.margolis@gmail.com>
2017-05-15 21:36:26 +0300

    EdgeIdProvider

--- fuse-axis2/fuse-unipop/src/main/java/com/kayhut/fuse/unipop/controller/utils/idProvider/EdgeIdProvider.java
+++ fuse-axis2/fuse-unipop/src/main/java/com/kayhut/fuse/unipop/controller/utils/idProvider/EdgeIdProvider.java
@@ -1,7 +1,12 @@
-package com.kayhut.fuse.unipop.controller.utils;
+package com.kayhut.fuse.unipop.controller.utils.idProvider;
+
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import java.util.Map;
 
 /**
  * Created by Roman on 15/05/2017.
  */
-public class EdgeIdProvider {
+public interface EdgeIdProvider<T>  {
+    T get(String edgeLabel, Vertex outV, Vertex inV, Map<String, Object> properties);
 }
