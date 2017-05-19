f509ca435f3f96871e1a7d11efd4731320b61fea
shimonbi <shimonbi@kayhut.com>
2017-05-19 13:04:33 +0300

    clean-up

--- fuse-datagen/src/main/java/com/kayhut/fuse/generator/model/entity/EntityBase.java
+++ fuse-datagen/src/main/java/com/kayhut/fuse/generator/model/entity/EntityBase.java
@@ -24,7 +24,7 @@ public abstract class EntityBase {
     }
     //endregion
 
-    //region Public Methods
+    //region Abstract Methods
     public abstract String[] getRecord();
     //endregion
 
