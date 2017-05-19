f8c18659a2335451bcb76c37403a9bb373fef9d7
shimonbi <shimonbi@kayhut.com>
2017-05-19 15:41:21 +0300

    Fixes

---
+++ fuse-datagen/src/test/java/com/kayhut/fuse/generator/DragonGeneratorTest.java
@@ -0,0 +1,39 @@
+package com.kayhut.fuse.generator.generator;
+
+import com.kayhut.fuse.generator.generator.dragon.DragonConfiguration;
+import com.kayhut.fuse.generator.generator.dragon.DragonGenerator;
+import com.kayhut.fuse.generator.model.entity.Dragon;
+import com.kayhut.fuse.generator.configuration.DataGenConfiguration;
+import org.apache.commons.configuration.Configuration;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by benishue on 15-May-17.
+ */
+public class DragonGeneratorTest {
+
+    static final String CONFIGURATION_FILE_PATH = "test.generator.properties";
+    static Configuration configuration;
+
+
+    @Test
+    public void generateDragon() throws Exception {
+        DragonGenerator dragonGenerator = new DragonGenerator(new DragonConfiguration(configuration));
+
+        for (int i= 0 ; i <100 ; i++) {
+            Dragon dragonA = dragonGenerator.generateDragon();
+            assertTrue(dragonA.getPower() >= 10 && dragonA.getPower() <= 100);
+            assertTrue(!dragonA.getName().isEmpty());
+            System.out.println(dragonA);
+        }
+
+    }
+    @BeforeClass
+    public static void setup() throws Exception {
+
+        configuration = new DataGenConfiguration(CONFIGURATION_FILE_PATH).getInstance();
+    }
+}
