84c71c179789a8df22b8b6e6cb92133317c77cfb
shimonbi <shimonbi@kayhut.com>
2017-05-24 01:28:12 +0300

    Fixes for stat

---
+++ fuse-stat/src/main/java/com/kayhut/fuse/stat/model/result/StatTermResult.java
@@ -0,0 +1,7 @@
+package com.kayhut.fuse.stat.model.result;
+
+/**
+ * Created by benishue on 24/05/2017.
+ */
+public class StatTermResult {
+}
---
+++ fuse-stat/src/test/java/com/kayhut/fuse/stat/util/EsUtilTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by benishue on 24/05/2017.
+ */
+public class EsUtilTest {
+
+}
