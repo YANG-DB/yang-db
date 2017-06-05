e967df56f53128fdd3f484ce209b9876358c4d9f
Yaniv Eliash <yaniv@kayhut.com>
2017-06-05 15:23:49 +0300

    Added Dockerfile and Jenkins build script

---
+++ jenkins_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mvn clean package
+docker login nexuss.westeurope.cloudapp.azure.com:5000 --username admin --password admin123
+docker build -t nexuss.westeurope.cloudapp.azure.com:5000/fuse-engine:latest .
+docker push nexuss.westeurope.cloudapp.azure.com:5000/fuse-engine:latest
