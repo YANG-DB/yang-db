b1b75da9c7409af78b64861cce59559db77d93b5
liorp <lior.perry@kayhut.com>
2017-06-08 12:13:14 +0300

    add more asg query validations

--- fuse-validator/src/main/java/com/kayhut/fuse/asg/strategy/validation/AsgStartEntityValidatorStrategy.java
+++ fuse-validator/src/main/java/com/kayhut/fuse/asg/strategy/validation/AsgStartEntityValidatorStrategy.java
@@ -7,6 +7,9 @@ import com.kayhut.fuse.model.asgQuery.AsgEBase;
 import com.kayhut.fuse.model.asgQuery.AsgQuery;
 import com.kayhut.fuse.model.asgQuery.AsgStrategyContext;
 import com.kayhut.fuse.model.ontology.Ontology;
+import com.kayhut.fuse.model.query.EBase;
+import com.kayhut.fuse.model.query.Rel;
+import com.kayhut.fuse.model.query.Start;
 import com.kayhut.fuse.model.query.entity.EConcrete;
 import com.kayhut.fuse.model.query.entity.EEntityBase;
 import com.kayhut.fuse.model.query.entity.ETyped;
@@ -14,14 +17,18 @@ import com.kayhut.fuse.model.query.entity.EUntyped;
 import com.kayhut.fuse.model.query.properties.EProp;
 import javaslang.collection.Stream;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static com.kayhut.fuse.asg.strategy.ValidationContext.OK;
+import static com.kayhut.fuse.dispatcher.utils.AsgQueryUtil.elements;
 
 public class AsgStartEntityValidatorStrategy implements AsgValidatorStrategy {
 
     public static final String ERROR_1 = "Ontology Name doesn't match query Ontology reference";
     public static final String ERROR_2 = "No Elements After Start Node";
+    public static final String ERROR_3 = "Start Node must be first element";
 
     @Override
     public ValidationContext apply(AsgQuery query, AsgStrategyContext context) {
@@ -31,6 +38,14 @@ public class AsgStartEntityValidatorStrategy implements AsgValidatorStrategy {
             return new ValidationContext(false, ERROR_2);
         if(!query.getOnt().equals(accessor.name()))
             return new ValidationContext(false, ERROR_1);
+
+        List<AsgEBase<EBase>> list = elements(query.getStart(), (asgEBase -> Collections.emptyList()), AsgEBase::getNext,
+                e -> e.geteBase() instanceof Start,
+                asgEBase -> true, Collections.emptyList());
+
+        if(!list.isEmpty())
+            return new ValidationContext(false, ERROR_3);
+
 
         return OK;
     }
--- fuse-validator/src/main/java/com/kayhut/fuse/asg/strategy/validation/AsgStepsValidatorStrategy.java
+++ fuse-validator/src/main/java/com/kayhut/fuse/asg/strategy/validation/AsgStepsValidatorStrategy.java
@@ -6,16 +6,13 @@ import com.kayhut.fuse.dispatcher.utils.AsgQueryUtil;
 import com.kayhut.fuse.model.Utils;
 import com.kayhut.fuse.model.asgQuery.AsgQuery;
 import com.kayhut.fuse.model.asgQuery.AsgStrategyContext;
-import com.kayhut.fuse.model.execution.plan.EntityFilterOp;
-import com.kayhut.fuse.model.execution.plan.EntityOp;
-import com.kayhut.fuse.model.execution.plan.RelationFilterOp;
-import com.kayhut.fuse.model.execution.plan.RelationOp;
-import com.kayhut.fuse.model.ontology.Ontology;
-import com.kayhut.fuse.model.query.EBase;
+import com.kayhut.fuse.model.query.Rel;
 import com.kayhut.fuse.model.query.entity.EEntityBase;
 import com.kayhut.fuse.model.query.properties.EPropGroup;
-import com.kayhut.fuse.model.results.Entity;
+import com.kayhut.fuse.model.query.properties.RelPropGroup;
+import com.sun.org.apache.regexp.internal.RE;
 
+import javax.management.relation.Relation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,24 +21,32 @@ import static com.kayhut.fuse.asg.strategy.ValidationContext.OK;
 
 public class AsgStepsValidatorStrategy implements AsgValidatorStrategy {
 
-    public static final String ERROR_1 = "Ontology Contains two adjacent steps without relation inside ";
-
-    static final String ENTITY_ONE = "entityOne";
-    static final String OPTIONAL_ENTITY_ONE_FILTER = "optionalEntityOneFilter";
-    static final String ENTITY_TWO = "entityTwo";
-    static final String OPTIONAL_ENTITY_TWO_FILTER = "optionalEntityTwoFilter";
+    public static final String ERROR_1 = "Ontology Contains two adjacent Entities without relation inside ";
+    public static final String ERROR_2 = "Ontology Contains two adjacent Relations without Entity inside ";
 
 
-    public static final String FULL_STEP = "(?<" + ENTITY_ONE + ">" + EEntityBase.class.getSimpleName() + ")" + ":" + "(?<" + OPTIONAL_ENTITY_ONE_FILTER + ">" + EPropGroup.class.getSimpleName() + ":)?" +
-            "(?<" + ENTITY_TWO + ">" + EEntityBase.class.getSimpleName() + ")" + "(:" + "(?<" + OPTIONAL_ENTITY_TWO_FILTER + ">" + EPropGroup.class.getSimpleName() + "))?";
+    public static final String REL = Relation.class.getSimpleName();
+    public static final String REL_PROP = RelPropGroup.class.getSimpleName();
+
+    public static final String ENTITY = EEntityBase.class.getSimpleName();
+    public static final String ENTITY_PROPS = EPropGroup.class.getSimpleName();
+
+
+    public static final String FULL_STEP_DOUBLE_REL = REL+"((:"+REL_PROP+")?):"+REL+"((:"+REL_PROP+")?)";
+
+    public static final String FULL_STEP_DOUBLE_ENTITY = ENTITY+"((:"+ENTITY_PROPS+")?):"+ENTITY+"((:"+ENTITY_PROPS+")?)";
 
     @Override
     public ValidationContext apply(AsgQuery query, AsgStrategyContext context) {
         List<String> errors = new ArrayList<>();
         String pattern = AsgQueryUtil.pattern(query);
-        Optional<String> match = Utils.match(pattern, FULL_STEP);
-        if(match.isPresent())
-            errors.add(ERROR_1 +":" + match.get());
+        Optional<String> match1 = Utils.match(pattern, FULL_STEP_DOUBLE_ENTITY);
+        if(match1.isPresent())
+            errors.add(ERROR_1 +":" + match1.get());
+
+        Optional<String> match2 = Utils.match(pattern,FULL_STEP_DOUBLE_REL);
+        if(match2.isPresent())
+            errors.add(ERROR_2 +":" + match2.get());
 
         if (errors.isEmpty())
             return OK;
