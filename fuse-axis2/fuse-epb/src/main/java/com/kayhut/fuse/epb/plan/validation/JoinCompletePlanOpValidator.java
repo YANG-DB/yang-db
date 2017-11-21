fb2cffebff3d5bfbafe20000d3ac397ca152e05c
moti cohen <mordechaic@kayhut.com>
2017-11-21 16:00:46 +0200

    Adding join validators

---
+++ fuse-axis2/fuse-epb/src/main/java/com/kayhut/fuse/epb/plan/validation/JoinCompletePlanOpValidator.java
@@ -0,0 +1,92 @@
+package com.kayhut.fuse.epb.plan.validation;
+
+import com.kayhut.fuse.dispatcher.utils.PlanUtil;
+import com.kayhut.fuse.dispatcher.utils.ValidationContext;
+import com.kayhut.fuse.epb.plan.PlanValidator;
+import com.kayhut.fuse.model.asgQuery.AsgQuery;
+import com.kayhut.fuse.model.execution.plan.EntityFilterOp;
+import com.kayhut.fuse.model.execution.plan.EntityJoinOp;
+import com.kayhut.fuse.model.execution.plan.EntityOp;
+import com.kayhut.fuse.model.execution.plan.Plan;
+import com.kayhut.fuse.model.log.Trace;
+import javaslang.Tuple2;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+
+import static com.kayhut.fuse.model.execution.plan.Plan.toPattern;
+
+/**
+ * Created by benishue on 7/4/2017.
+ */
+public class JoinCompletePlanOpValidator implements PlanValidator<Plan, AsgQuery> {
+    private Trace<String> trace = Trace.build(JoinCompletePlanOpValidator.class.getSimpleName());
+
+    //region PlanValidator Implementation
+    @Override
+    public void log(String event, Level level) {
+        trace.log(event, level);
+    }
+
+    @Override
+    public List<Tuple2<String, String>> getLogs(Level level) {
+        return trace.getLogs(level);
+    }
+
+    @Override
+    public String who() {
+        return trace.who();
+    }
+
+    @Override
+    public ValidationContext isPlanValid(Plan plan, AsgQuery query) {
+        Optional<EntityJoinOp> joinOp = PlanUtil.first(plan, EntityJoinOp.class);
+        /*
+        We need to check validity only if the # of Ops is at least 2, if so, we need to check
+        that this JoinOp is a 'Complete' one (See function below).
+        The 'Extenders' (according to Mr. R) will always make sure that the JoinOp will be the first element.
+        */
+        if (plan.getOps().size() > 1 && joinOp.isPresent() && PlanUtil.isFirst(plan, joinOp.get())
+                && !isJoinOpComplete(joinOp.get())) {
+            return new ValidationContext(false, "JoinOp complete validation failed: " + toPattern(plan));
+        }
+        return ValidationContext.OK;
+    }
+    //endregion
+
+    //region Private Methods
+
+    /*
+     * "Complete Join Op" - on the left branch of the JoinOp we are looking
+     * for the last EntityOp(EOP) or EOP + attached EntityFilterOp (EFO).
+     * We should check that we have this EOP (or EOP + EFO) at the right branch of the JoinOp.
+     * i.e., The enums should be the same.
+     */
+    private boolean isJoinOpComplete(EntityJoinOp joinOp) {
+        Optional<EntityOp> leftEntityOp = PlanUtil.last(joinOp.getLeftBranch(), EntityOp.class);
+        if (!leftEntityOp.isPresent()) {
+            return false;
+        }
+
+        Optional<EntityOp> rightEntityOp = PlanUtil.<EntityOp>first(joinOp.getRightBranch(),
+                op -> (op.geteNum() == leftEntityOp.get().geteNum()));
+
+        if (!rightEntityOp.isPresent()) {
+            return false;
+        }
+
+        Optional<EntityFilterOp> leftEntityFilterOp = PlanUtil.next(joinOp.getLeftBranch(), leftEntityOp.get(), EntityFilterOp.class);
+        if (leftEntityFilterOp.isPresent()) {
+            Optional<EntityFilterOp> rightEntityFilterOp = PlanUtil.next(joinOp.getRightBranch(), rightEntityOp.get(), EntityFilterOp.class);
+            if (!rightEntityFilterOp.isPresent() ||
+                    rightEntityFilterOp.get().geteNum() != leftEntityFilterOp.get().geteNum()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //endregion
+
+}
---
+++ fuse-axis2/fuse-epb/src/main/java/com/kayhut/fuse/epb/plan/validation/JoinIntersectionPlanOpValidator.java
@@ -0,0 +1,94 @@
+package com.kayhut.fuse.epb.plan.validation;
+
+import com.kayhut.fuse.dispatcher.utils.PlanUtil;
+import com.kayhut.fuse.dispatcher.utils.ValidationContext;
+import com.kayhut.fuse.epb.plan.PlanValidator;
+import com.kayhut.fuse.model.asgQuery.AsgQuery;
+import com.kayhut.fuse.model.execution.plan.EntityJoinOp;
+import com.kayhut.fuse.model.execution.plan.EntityOp;
+import com.kayhut.fuse.model.execution.plan.Plan;
+import com.kayhut.fuse.model.execution.plan.PlanOpBase;
+import com.kayhut.fuse.model.log.Trace;
+import javaslang.Tuple2;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.logging.Level;
+
+import static com.kayhut.fuse.model.execution.plan.Plan.toPattern;
+
+/**
+ * Created by benishue on 7/5/2017.
+ */
+public class JoinIntersectionPlanOpValidator implements PlanValidator<Plan, AsgQuery> {
+    private Trace<String> trace = Trace.build(JoinIntersectionPlanOpValidator.class.getSimpleName());
+
+    //region PlanValidator Implementation
+    @Override
+    public void log(String event, Level level) {
+        trace.log(event, level);
+    }
+
+    @Override
+    public List<Tuple2<String, String>> getLogs(Level level) {
+        return trace.getLogs(level);
+    }
+
+    @Override
+    public String who() {
+        return trace.who();
+    }
+
+    @Override
+    public ValidationContext isPlanValid(Plan plan, AsgQuery query) {
+        Optional<EntityJoinOp> joinOp = PlanUtil.first(plan, EntityJoinOp.class);
+        /*
+        We need to check validity only if the # of Ops is exactly 1 and the Op is JoinOp,
+        if so, we need to check that the intersection of the Join branches (left, right)
+        is 0 or 1.
+        */
+        if (plan.getOps().size() == 1 && joinOp.isPresent() && !isIntersectionValid(joinOp.get())) {
+            return new ValidationContext(false, "JoinOp intersection validation failed: " + toPattern(plan));
+        }
+        return ValidationContext.OK;
+    }
+    //endregion
+
+    //region Private Methods
+
+    private boolean isIntersectionValid(EntityJoinOp joinOp) {
+        Set<Integer> leftEopSet = getEntityOpsRecursively(joinOp.getLeftBranch().getOps(), new HashSet<>());
+        Set<Integer> rightEopSet = getEntityOpsRecursively(joinOp.getRightBranch().getOps(), new HashSet<>());
+
+        Set<Integer> intersection = new HashSet<>(leftEopSet);
+        intersection.retainAll(rightEopSet);
+
+        //0 intersection is OK, since we can be in a state where we didn't finish yet.
+        if (intersection.size() == 0) {
+            return true;
+        }
+        if (intersection.size() == 1) {
+            return (joinOp.getAsgEBase().geteNum() == intersection.iterator().next());
+        }
+
+        return false;
+    }
+
+    private Set<Integer> getEntityOpsRecursively(List<PlanOpBase> ops, Set<Integer> set) {
+        for (PlanOpBase op : ops) {
+            if (op instanceof EntityOp) {
+                set.add(op.geteNum());
+            }
+            if (op instanceof EntityJoinOp) {
+                getEntityOpsRecursively(((EntityJoinOp) op).getLeftBranch().getOps(), set);
+                getEntityOpsRecursively(((EntityJoinOp) op).getRightBranch().getOps(), set);
+            }
+        }
+        return set;
+    }
+
+    //endregion
+
+}
