af69a77346cd42ce85233df4a4a9d77fc42d9e45
moti cohen <mordechaic@kayhut.com>
2017-11-22 15:33:04 +0200

    Adding join extension tests

---
+++ fuse-axis2/fuse-epb/src/test/java/com/kayhut/fuse/epb/plan/extenders/OngoingJoinStrategyTest.java
@@ -0,0 +1,90 @@
+package com.kayhut.fuse.epb.plan.extenders;
+
+import com.kayhut.fuse.dispatcher.utils.AsgQueryUtil;
+import com.kayhut.fuse.model.asgQuery.AsgEBase;
+import com.kayhut.fuse.model.asgQuery.AsgQuery;
+import com.kayhut.fuse.model.execution.plan.EntityOp;
+import com.kayhut.fuse.model.execution.plan.Plan;
+import com.kayhut.fuse.model.query.Rel;
+import com.kayhut.fuse.model.query.Start;
+import com.kayhut.fuse.model.query.entity.ETyped;
+import javaslang.collection.Stream;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static com.kayhut.fuse.model.query.Rel.Direction.R;
+
+/**
+ * Created by moti on 04/07/2017.
+ */
+public class OngoingJoinStrategyTest {
+    public static AsgQuery simpleQuery1(String queryName, String ontologyName) {
+        Start start = new Start();
+        start.seteNum(0);
+
+        ETyped eTyped = new ETyped();
+        eTyped.seteNum(1);
+        eTyped.seteTag("A");
+        eTyped.seteType("1");
+
+        Rel rel = new Rel();
+        rel.seteNum(2);
+        rel.setDir(R);
+        rel.setrType("1");
+
+        ETyped eTyped2 = new ETyped();
+        eTyped2.seteNum(3);
+        eTyped2.seteTag("B");
+        eTyped2.seteType("2");
+
+        AsgEBase<Start> asgStart =
+                AsgEBase.Builder.<Start>get().withEBase(start)
+                        .withNext(AsgEBase.Builder.get().withEBase(eTyped)
+                                .withNext(AsgEBase.Builder.get().withEBase(rel)
+                                        .withNext(AsgEBase.Builder.get().withEBase(eTyped2)
+                                                .build())
+                                        .build())
+                                .build())
+                        .build();
+
+        return AsgQuery.AsgQueryBuilder.anAsgQuery().withName(queryName).withOnt(ontologyName).withStart(asgStart).build();
+    }
+
+    @Test
+    public void emptyPlanTest(){
+        AsgQuery asgQuery = simpleQuery1("name", "ont");
+        M1NonRedundantPlanExtensionStrategy m1Strategy = new M1NonRedundantPlanExtensionStrategy();
+        JoinOngoingExtensionStrategy join = new JoinOngoingExtensionStrategy(m1Strategy);
+        Iterable<Plan> plans = join.extendPlan(Optional.empty(), asgQuery);
+        Assert.assertEquals(0, Stream.ofAll(plans).length());
+    }
+
+    @Test
+    public void singleJoinPlanTest(){
+        AsgQuery asgQuery = simpleQuery1("name", "ont");
+        Plan plan = new Plan(new EntityOp(AsgQueryUtil.element$(asgQuery, 1)));
+
+        M1NonRedundantPlanExtensionStrategy m1ExtensionStrategy = new M1NonRedundantPlanExtensionStrategy();
+
+        JoinSeedExtensionStrategy join = new JoinSeedExtensionStrategy(m1ExtensionStrategy);
+        Iterable<Plan> plans = join.extendPlan(Optional.of(plan), asgQuery);
+
+        JoinOngoingExtensionStrategy joinOngoing = new JoinOngoingExtensionStrategy(m1ExtensionStrategy);
+        Iterable<Plan> joinExtensionPlans = joinOngoing.extendPlan(Optional.of(plans.iterator().next()), asgQuery);
+
+        Assert.assertEquals(1, Stream.ofAll(joinExtensionPlans).length());
+    }
+
+    @Test
+    public void singleEntityPlanTest(){
+        AsgQuery asgQuery = simpleQuery1("name", "ont");
+        Plan plan = new Plan(new EntityOp(AsgQueryUtil.element$(asgQuery, 1)));
+        M1NonRedundantPlanExtensionStrategy m1Strategy = new M1NonRedundantPlanExtensionStrategy();
+        JoinOngoingExtensionStrategy joinOngoing = new JoinOngoingExtensionStrategy(m1Strategy);
+        Iterable<Plan> plans = joinOngoing.extendPlan(Optional.of(plan), asgQuery);
+        Assert.assertEquals(0, Stream.ofAll(plans).length());
+    }
+
+}
