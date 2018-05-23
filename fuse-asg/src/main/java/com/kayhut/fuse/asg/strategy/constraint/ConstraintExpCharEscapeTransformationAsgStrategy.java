8eba224f6b953192f172785f907cd2dbca41f98d
liorp <lior.perry@kayhut.com>
2018-05-23 13:49:00 +0300

    Merge branch 'develop' of http://ec2-52-31-47-238.eu-west-1.compute.amazonaws.com/fuse/Fuse into develop
    # Conflicts:
    #	fuse-service/src/test/java/com/kayhut/fuse/services/engine2/RealClusterKnowledgeTest.java

---
+++ fuse-asg/src/main/java/com/kayhut/fuse/asg/strategy/constraint/ConstraintExpCharEscapeTransformationAsgStrategy.java
@@ -0,0 +1,92 @@
+package com.kayhut.fuse.asg.strategy.constraint;
+
+import com.kayhut.fuse.model.asgQuery.AsgQuery;
+import com.kayhut.fuse.model.asgQuery.AsgStrategyContext;
+import com.kayhut.fuse.model.ontology.Property;
+import com.kayhut.fuse.model.query.EBase;
+import com.kayhut.fuse.model.query.properties.EProp;
+import com.kayhut.fuse.model.query.properties.RelProp;
+import com.kayhut.fuse.model.query.properties.constraint.Constraint;
+import javaslang.collection.Stream;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.kayhut.fuse.model.query.properties.constraint.ConstraintOp.like;
+import static com.kayhut.fuse.model.query.properties.constraint.ConstraintOp.likeAny;
+
+/**
+ * checks all constraints with op = like / like_any and adds to all expression's special chars '\\' is not present
+ * this strategy must be the last since there are prior strategies that transforms like to eq (performance optimization)
+ * therefore we don't want to change the expression when it is not going to be a "wildcard" search
+ * we also need to do the expression altering only on string type properties so we need to wait for
+ * the ConstraintTypeTransformationStrategy to be applied first
+ */
+public class ConstraintExpCharEscapeTransformationAsgStrategy extends ConstraintTransformationAsgStrategyBase {
+
+    @Override
+    public void apply(AsgQuery query, AsgStrategyContext context) {
+
+        getEprops(query).forEach(eProp -> {
+            applyExpressionTransformation(context, eProp, EProp.class);
+        });
+
+        getRelProps(query).forEach(relProp -> {
+            applyExpressionTransformation(context, relProp, RelProp.class);
+        });
+    }
+
+    //region Private Methods
+
+    private void applyExpressionTransformation(AsgStrategyContext context, EBase eBase, Class klass) {
+        if (klass == EProp.class || klass == RelProp.class) {
+            EProp eProp = (EProp) eBase;
+            Optional<Property> property = context.getOntologyAccessor().$property(eProp.getpType());
+            final Constraint con = eProp.getCon();
+            if (property.isPresent()
+                    //verify constraint of wildcard type
+                    && (con.getOp().equals(like) || con.getOp().equals(likeAny))
+                    //verify expression of string type
+                    && property.get().getType().equals("string")) {
+                con.setExpr(escapeSpecialChars(con.getExpr()));
+            }
+        }
+    }
+
+    private Object escapeSpecialChars(Object expr) {
+        if (expr instanceof List) {
+            return Stream.ofAll(((List) expr)).map(p -> escape(p)).toJavaList();
+        }
+        return escape(expr);
+    }
+
+    /**
+     * Returns a String where those characters that QueryParser
+     * expects to be escaped are escaped by a preceding <code>\</code>.
+     */
+    public static Object escape(Object s) {
+        if (s instanceof String) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < ((String) s).length(); i++) {
+                char c = ((String) s).charAt(i);
+                // These characters are part of the query syntax and must be escaped
+                if (c == '\\' || c == '+' || c == '-' || c == '!' || c == '(' || c == ')' || c == ':'
+                        || c == '^' || c == '[' || c == ']' || c == '\"' || c == '{' || c == '}' || c == '~'
+                        /* || c == '*' */ || c == '?' || c == '|' || c == '&' || c == '/') {
+                    sb.append('\\');
+                }
+                sb.append(c);
+            }
+            return sb.toString();
+        }
+        //do nothing
+        return s;
+    }
+
+    //endregion
+
+}
+
+
+
+
