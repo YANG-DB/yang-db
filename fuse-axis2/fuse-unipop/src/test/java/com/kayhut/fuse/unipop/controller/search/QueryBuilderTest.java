a2236035871005bfeb93443604a6a55076d20d37
liorp <lior.perry@kayhut.com>
2018-05-29 09:08:17 +0300

    fix minor issue of testing for empty filter in KnowLikeCombinerStrategy

---
+++ fuse-axis2/fuse-unipop/src/test/java/com/kayhut/fuse/unipop/controller/search/QueryBuilderTest.java
@@ -0,0 +1,27 @@
+package com.kayhut.fuse.unipop.controller.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * test query builder composit expressions
+ */
+public class QueryBuilderTest {
+
+    @Test
+    public void wildcardScriptTest() {
+        final QueryBuilder queryBuilder = new QueryBuilder();
+        queryBuilder.query();
+        queryBuilder.bool();
+        queryBuilder.should();
+        queryBuilder.wildcardScript("StringValue.keyword","*blabla*");
+
+
+        Assert.assertTrue(queryBuilder.getCurrent() instanceof QueryBuilder.ScriptComposite);
+        final QueryBuilder.ScriptComposite current = (QueryBuilder.ScriptComposite) queryBuilder.getCurrent();
+        Assert.assertEquals("wildcard", current.getName());
+        Assert.assertEquals("*blabla*", current.getValue());
+        Assert.assertEquals("StringValue.keyword", current.getFieldName());
+        Assert.assertEquals(QueryBuilder.Op.script, current.getOp());
+    }
+}
