34531a996bddeaba566a2b0a1b4a6e493323f526
liorp <lior.perry@tikalk.com>
2018-06-27 16:34:02 +0300

    Merge remote-tracking branch 'remotes/origin/release/0.3.1' into grapgstream
    # Conflicts:
    #	data-gen/pom.xml
    #	fuse-asg/pom.xml
    #	fuse-assembly/pom.xml
    #	fuse-axis1/fuse-neo4j/pom.xml
    #	fuse-axis1/pom.xml
    #	fuse-axis2/fuse-epb/pom.xml
    #	fuse-axis2/fuse-executor/pom.xml
    #	fuse-axis2/fuse-gta/pom.xml
    #	fuse-axis2/fuse-stat/pom.xml
    #	fuse-axis2/fuse-unipop-asg/pom.xml
    #	fuse-axis2/fuse-unipop/pom.xml
    #	fuse-axis2/pom.xml
    #	fuse-datagen/pom.xml
    #	fuse-dispatcher/pom.xml
    #	fuse-model/pom.xml
    #	fuse-service/pom.xml
    #	fuse-test/dragons-test-framework/pom.xml
    #	fuse-test/fuse-benchmarks-test/pom.xml
    #	fuse-test/fuse-test-framework/pom.xml
    #	fuse-test/pom.xml
    #	fuse-validator/pom.xml
    #	geojson-jackson/pom.xml
    #	pom.xml

---
+++ fuse-asg/src/main/java/com/kayhut/fuse/asg/strategy/AsgOntologyRelValidatorStrategy.java
@@ -0,0 +1,90 @@
+package com.kayhut.fuse.asg.strategy;
+
+import com.kayhut.fuse.dispatcher.utils.AsgQueryUtil;
+import com.kayhut.fuse.model.asgQuery.AsgEBase;
+import com.kayhut.fuse.model.asgQuery.AsgQuery;
+import com.kayhut.fuse.model.asgQuery.AsgStrategyContext;
+import com.kayhut.fuse.model.ontology.EPair;
+import com.kayhut.fuse.model.ontology.Ontology;
+import com.kayhut.fuse.model.query.Rel;
+import com.kayhut.fuse.model.query.entity.EEntityBase;
+import com.kayhut.fuse.model.query.entity.EUntyped;
+import com.kayhut.fuse.model.query.entity.Typed;
+import com.kayhut.fuse.model.validation.ValidationResult;
+import javaslang.collection.HashSet;
+import javaslang.collection.Set;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.kayhut.fuse.dispatcher.utils.AsgQueryUtil.elements;
+import static com.kayhut.fuse.model.validation.ValidationResult.OK;
+
+public class AsgOntologyRelValidatorStrategy implements AsgValidatorStrategy {
+    public static final String ERROR_1 = "Ontology doesn't Allow Relation with No entity Attached to ";
+    public static final String ERROR_2 = "Ontology doesn't Allow such Relation with Entities construct ";
+
+    @Override
+    public ValidationResult apply(AsgQuery query, AsgStrategyContext context) {
+        List<String> errors = new ArrayList<>();
+        Ontology.Accessor accessor = context.getOntologyAccessor();
+        List<AsgEBase<Rel>> list = elements(query.getStart(),
+                (asgEBase -> Collections.emptyList()), AsgEBase::getNext,
+                e -> e.geteBase() instanceof Rel,
+                asgEBase -> true, Collections.emptyList());
+
+        list.forEach(rel -> {
+            Rel.Direction dir = rel.geteBase().getDir();
+
+            Optional<AsgEBase<EEntityBase>> sideA = AsgQueryUtil.ancestor(rel, v -> v.geteBase() instanceof EEntityBase);
+            Optional<AsgEBase<EEntityBase>> sideB = AsgQueryUtil.nextAdjacentDescendant(rel, EEntityBase.class);
+
+            if (dir.equals(Rel.Direction.L)) {
+                sideB = AsgQueryUtil.ancestor(rel, v -> v.geteBase() instanceof EEntityBase);
+                sideA = AsgQueryUtil.nextAdjacentDescendant(rel, EEntityBase.class);
+            }
+
+            if (!sideA.isPresent() || !sideB.isPresent())
+                errors.add(ERROR_1 + ":" + rel);
+
+            List<EPair> elements = accessor.$relation$(rel.geteBase().getrType()).getePairs();
+            Set<String> allowedSideA = HashSet.<String>of().addAll(elements.stream().map(EPair::geteTypeA).collect(Collectors.toList()));
+            Set<String> allowedSideB = HashSet.<String>of().addAll(elements.stream().map(EPair::geteTypeB).collect(Collectors.toList()));
+
+            Set<String> sideATypes = getSideTypes(sideA);
+            Set<String> sideBTypes = getSideTypes(sideB);
+
+
+            if (!sideATypes.isEmpty()) {
+                if(allowedSideA.intersect(sideATypes).size() < sideATypes.size())
+                    errors.add(ERROR_2 + ":" + ValidationResult.print(sideA.get(), rel, sideB.get()));
+            }
+
+            if (!sideBTypes.isEmpty()) {
+                if(allowedSideB.intersect(sideBTypes).size() < sideBTypes.size())
+                    errors.add(ERROR_2 + ":" + ValidationResult.print(sideA.get(), rel, sideB.get()));
+            }
+        });
+
+        if (errors.isEmpty())
+            return OK;
+
+        return new ValidationResult(false, errors.toArray(new String[errors.size()]));
+    }
+
+    private Set<String> getSideTypes(Optional<AsgEBase<EEntityBase>> side) {
+        Set<String> hashSet = HashSet.of();
+        if (side.isPresent()) {
+            if (Typed.eTyped.class.isAssignableFrom(side.get().geteBase().getClass())) {
+                hashSet = hashSet.add(((Typed.eTyped) side.get().geteBase()).geteType());
+            } else if (EUntyped.class.isAssignableFrom(side.get().geteBase().getClass())) {
+                hashSet = hashSet.addAll(((EUntyped) side.get().geteBase()).getvTypes());
+            }
+        }
+        return hashSet;
+    }
+    //endregion
+}
---
+++ fuse-asg/src/test/java/com/kayhut/fuse/asg/strategy/validation/AsgOntologyRelValidatorStrategyTest.java
@@ -0,0 +1,232 @@
+package com.kayhut.fuse.asg.strategy.validation;
+
+import com.kayhut.fuse.asg.strategy.AsgOntologyRelValidatorStrategy;
+import com.kayhut.fuse.model.OntologyTestUtils;
+import com.kayhut.fuse.model.asgQuery.AsgQuery;
+import com.kayhut.fuse.model.asgQuery.AsgStrategyContext;
+import com.kayhut.fuse.model.ontology.Ontology;
+import com.kayhut.fuse.model.query.Rel;
+import com.kayhut.fuse.model.query.properties.EProp;
+import com.kayhut.fuse.model.query.properties.RelProp;
+import com.kayhut.fuse.model.query.properties.constraint.Constraint;
+import com.kayhut.fuse.model.query.properties.constraint.ConstraintOp;
+import com.kayhut.fuse.model.query.quant.QuantType;
+import com.kayhut.fuse.model.validation.ValidationResult;
+import javaslang.collection.Stream;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static com.kayhut.fuse.model.OntologyTestUtils.*;
+import static com.kayhut.fuse.model.asgQuery.AsgQuery.Builder.*;
+import static com.kayhut.fuse.model.query.Rel.Direction.R;
+import static com.kayhut.fuse.model.query.properties.constraint.Constraint.of;
+import static com.kayhut.fuse.model.query.properties.constraint.ConstraintOp.eq;
+
+/**
+ * Created by liorp on 6/6/2017.
+ */
+public class AsgOntologyRelValidatorStrategyTest {
+    Ontology ontology;
+
+    AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons")
+            .next(typed(1, PERSON.type))
+            .next(rel(2, OntologyTestUtils.OWN.getrType(), R).below(relProp(10,
+                    RelProp.of(10, START_DATE.type, of(eq, new Date())))))
+            .next(concrete(3, "HorseWithNoName", HORSE.type, "display", "eTag"))
+            .build();
+
+    @Before
+    public void setUp() throws Exception {
+        ontology = OntologyTestUtils.createDragonsOntologyLong();
+    }
+
+    @Test
+    public void testValidParentEntityWithQuantUntypedQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons").
+                next(unTyped(1, DRAGON.type, HORSE.type)).
+                next(quant1(2, QuantType.all)).
+                in(eProp(3, EProp.of(3, GENDER.type, Constraint.of(ConstraintOp.eq, Gender.MALE))),
+                        rel(8, REGISTERED.getrType(), Rel.Direction.R).below(relProp(9)).
+                                next(typed(10, GUILD.type)
+                                        .next(eProp(11,
+                                                EProp.of(11, NAME.type, Constraint.of(ConstraintOp.eq, "abc")))))).
+                build();
+
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertTrue(validationResult.valid());
+    }
+    @Test
+    public void testNotValidDirectionParentEntityWithQuantUntypedQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons").
+                next(unTyped(1, DRAGON.type, HORSE.type)).
+                next(quant1(2, QuantType.all)).
+                in(eProp(3, EProp.of(3, GENDER.type, Constraint.of(ConstraintOp.eq, Gender.MALE))),
+                        rel(8, REGISTERED.getrType(), Rel.Direction.L).below(relProp(9)).
+                                next(typed(10, GUILD.type)
+                                        .next(eProp(11,
+                                                EProp.of(11, NAME.type, Constraint.of(ConstraintOp.eq, "abc")))))).
+                build();
+
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertFalse(validationResult.valid());
+        Assert.assertTrue(Stream.ofAll(validationResult.errors()).toJavaArray(String.class)[0].contains(AsgOntologyRelValidatorStrategy.ERROR_2));
+    }
+
+    @Test
+    public void testValidParentEntityWithQuantNonValidUntypedQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons").
+                next(unTyped(1, DRAGON.type, HORSE.type)).
+                next(quant1(2, QuantType.all)).
+                in(eProp(3, EProp.of(3, GENDER.type, Constraint.of(ConstraintOp.eq, Gender.MALE))),
+                        rel(8, REGISTERED.getrType(), Rel.Direction.R).below(relProp(9)).
+                                next(typed(10, GUILD.type)
+                                        .next(eProp(11,
+                                                EProp.of(11, NAME.type, Constraint.of(ConstraintOp.eq, "abc")))))).
+                build();
+
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertTrue(validationResult.valid());
+    }
+
+    @Test
+    public void testValidParentEntityWithQuantReverseQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons").
+                next(typed(1, DRAGON.type)).
+                next(quant1(2, QuantType.all)).
+                in(eProp(3, EProp.of(3, NAME.type, Constraint.of(ConstraintOp.le, "abc"))),
+                        rel(8, FIRE.getrType(), Rel.Direction.L).below(relProp(9)).
+                                next(typed(10, DRAGON.type)
+                                        .next(eProp(11, EProp.of(11, NAME.type, Constraint.of(ConstraintOp.eq, "abc"))))),
+                        rel(4, FREEZE.getrType(), Rel.Direction.R).below(relProp(5)).
+                                next(typed(6, DRAGON.type)
+                                        .next(eProp(7, EProp.of(7, NAME.type, Constraint.of(ConstraintOp.eq, "abc")))))).
+                build();
+
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertTrue(validationResult.valid());
+    }
+
+    @Test
+    public void testValidParentEntityWithQuantQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons").
+                next(typed(1, DRAGON.type)).
+                next(quant1(2, QuantType.all)).
+                in(eProp(3, EProp.of(3, NAME.type, Constraint.of(ConstraintOp.le, "abc"))),
+                        rel(8, FIRE.getrType(), Rel.Direction.R).below(relProp(9)).
+                                next(typed(10, DRAGON.type)
+                                        .next(eProp(11, EProp.of(11, NAME.type, Constraint.of(ConstraintOp.eq, "abc"))))),
+                        rel(4, FREEZE.getrType(), Rel.Direction.R).below(relProp(5)).
+                                next(typed(6, DRAGON.type)
+                                        .next(eProp(7, EProp.of(7, NAME.type, Constraint.of(ConstraintOp.eq, "abc")))))).
+                build();
+
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertTrue(validationResult.valid());
+    }
+
+    @Test
+    public void testNotValidNoParentEntityWithQuantQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons").
+                next(quant1(2, QuantType.all)).
+                in(eProp(3, EProp.of(3, NAME.type, Constraint.of(ConstraintOp.le, "abc"))),
+                        rel(8, FIRE.getrType(), Rel.Direction.R).below(relProp(9)).
+                                next(typed(10, DRAGON.type).next(eProp(11, EProp.of(11, NAME.type, Constraint.of(ConstraintOp.eq, "abc"))))),
+                        rel(4, FREEZE.getrType(), Rel.Direction.R).below(relProp(5)).
+                                next(typed(6, DRAGON.type)
+                                        .next(eProp(7, EProp.of(7, NAME.type, Constraint.of(ConstraintOp.eq, "abc")))))).
+                build();
+
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertFalse(validationResult.valid());
+        Assert.assertTrue(Stream.ofAll(validationResult.errors()).toJavaArray(String.class)[0].contains(AsgOntologyRelValidatorStrategy.ERROR_1));
+    }
+
+    @Test
+    public void testValidTypeQuery() {
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertTrue(validationResult.valid());
+    }
+
+    @Test
+    public void testValidUntypedOneSideQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons")
+                .next(unTyped(1))
+                .next(rel(2, OntologyTestUtils.OWN.getrType(), R).below(relProp(10,
+                        RelProp.of(10, START_DATE.type, of(eq, new Date())))))
+                .next(concrete(3, "HorseWithNoName", HORSE.type, "display", "eTag"))
+                .build();
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertTrue(validationResult.valid());
+    }
+
+    @Test
+    public void testValidUntypedTowSidesQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons")
+                .next(unTyped(1))
+                .next(rel(2, OntologyTestUtils.OWN.getrType(), R).below(relProp(10,
+                        RelProp.of(10, START_DATE.type, of(eq, new Date())))))
+                .next(unTyped(3))
+                .build();
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertTrue(validationResult.valid());
+    }
+
+    @Test
+    public void testNotValidRelWithOneSideMissingQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons")
+                .next(rel(2, OntologyTestUtils.OWN.getrType(), R).below(relProp(10,
+                        RelProp.of(10, START_DATE.type, of(eq, new Date())))))
+                .next(concrete(3, "HorseWithNoName", HORSE.type, "display", "eTag"))
+                .build();
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertFalse(validationResult.valid());
+        Assert.assertTrue(Stream.ofAll(validationResult.errors()).toJavaArray(String.class)[0].contains(AsgOntologyRelValidatorStrategy.ERROR_1));
+
+    }
+
+    @Test
+    public void testNotValidRelWithOneSideNotAllowedByRelTypesQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons")
+                .next(unTyped(1, PERSON.type))
+                .next(rel(2, OntologyTestUtils.FIRE.getrType(), R).below(relProp(10,
+                        RelProp.of(10, START_DATE.type, of(eq, new Date())))))
+                .next(concrete(3, "HorseWithNoName", HORSE.type, "display", "eTag"))
+                .build();
+
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertFalse(validationResult.valid());
+        Assert.assertTrue(Stream.ofAll(validationResult.errors()).toJavaArray(String.class)[0].contains(AsgOntologyRelValidatorStrategy.ERROR_2));
+
+    }
+
+    @Test
+    public void testNotValidRelWithTwoSidesNotAllowedByRelTypesQuery() {
+        AsgQuery query = AsgQuery.Builder.start("Q1", "Dragons")
+                .next(typed(1, PERSON.type))
+                .next(rel(2, OntologyTestUtils.FIRE.getrType(), R).below(relProp(10,
+                        RelProp.of(10, START_DATE.type, of(eq, new Date())))))
+                .next(concrete(3, "HorseWithNoName", HORSE.type, "display", "eTag"))
+                .build();
+
+        AsgOntologyRelValidatorStrategy strategy = new AsgOntologyRelValidatorStrategy();
+        ValidationResult validationResult = strategy.apply(query, new AsgStrategyContext(new Ontology.Accessor(ontology)));
+        Assert.assertFalse(validationResult.valid());
+        Assert.assertTrue(Stream.ofAll(validationResult.errors()).toJavaArray(String.class)[0].contains(AsgOntologyRelValidatorStrategy.ERROR_2));
+
+    }
+
+}
