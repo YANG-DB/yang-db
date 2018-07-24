20dd8a7ffddf748f9032fdb3fd8aab91d8377b43
kobi shaul <kobis@kayhut.com>
2018-07-24 14:40:33 +0300

    Evalue tests

---
+++ fuse-domain/fuse-domain-knowledge/fuse-domain-knowledge-test/src/test/java/com/kayhut/fuse/assembly/knowledge/KnowledgeSimpleEfileWithFilterE2ETests.java
@@ -0,0 +1,81 @@
+package com.kayhut.fuse.assembly.knowledge;
+
+import com.kayhut.fuse.assembly.knowledge.domain.FileBuilder;
+import com.kayhut.fuse.assembly.knowledge.domain.KnowledgeWriterContext;
+import com.kayhut.fuse.model.query.Query;
+import com.kayhut.fuse.model.query.Start;
+import com.kayhut.fuse.model.query.entity.ETyped;
+import com.kayhut.fuse.model.query.properties.EProp;
+import com.kayhut.fuse.model.query.properties.constraint.Constraint;
+import com.kayhut.fuse.model.query.properties.constraint.ConstraintOp;
+import com.kayhut.fuse.model.resourceInfo.FuseResourceInfo;
+import com.kayhut.fuse.model.results.Assignment;
+import com.kayhut.fuse.model.results.AssignmentsQueryResult;
+import com.kayhut.fuse.model.results.QueryResultAssert;
+import com.kayhut.fuse.model.results.QueryResultBase;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+import static com.kayhut.fuse.assembly.knowledge.Setup.client;
+import static com.kayhut.fuse.assembly.knowledge.Setup.fuseClient;
+import static com.kayhut.fuse.assembly.knowledge.Setup.manager;
+import static com.kayhut.fuse.assembly.knowledge.domain.EntityBuilder.INDEX;
+import static com.kayhut.fuse.assembly.knowledge.domain.FileBuilder._f;
+import static com.kayhut.fuse.assembly.knowledge.domain.KnowledgeReaderContext.KNOWLEDGE;
+import static com.kayhut.fuse.assembly.knowledge.domain.KnowledgeReaderContext.query;
+import static com.kayhut.fuse.assembly.knowledge.domain.KnowledgeWriterContext.commit;
+
+
+public class KnowledgeSimpleEfileWithFilterE2ETests {
+
+    static KnowledgeWriterContext ctx;
+    static FileBuilder f1;
+    static private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        Setup.setup();
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        ctx = KnowledgeWriterContext.init(client, manager.getSchema());
+        // Evalue entities for tests
+        f1 = _f(ctx.nextFileId()).name("kobi");
+        f1.logicalId = ctx.nextLogicalId();
+        Assert.assertEquals(1, commit(ctx, INDEX, f1));
+    }
+
+    @AfterClass
+    public static void after() {
+        ctx.removeCreated();
+    }
+
+    // Start Tests:
+    @Test
+    public void testEqEfileByLogicalId() throws IOException, InterruptedException
+    {
+        // Create v1 query to fetch newly created entity
+        FuseResourceInfo fuseResourceInfo = fuseClient.getFuseInfo();
+        Query query = Query.Builder.instance().withName("query").withOnt(KNOWLEDGE)
+                .withElements(Arrays.asList(
+                        new Start(0, 1),
+                        new ETyped(1, "A", "Efile", 2, 0),
+                        new EProp(2, "logicalId", Constraint.of(ConstraintOp.eq, f1.logicalId))
+                )).build();
+        QueryResultBase pageData = query(fuseClient, fuseResourceInfo, query);
+
+        // Create expectedResult
+        AssignmentsQueryResult expectedResult = AssignmentsQueryResult.Builder.instance()
+                .withAssignment(Assignment.Builder.instance()
+                        .withEntity(f1.toEntity())  //context entity
+                        .build())
+                .build();
+
+        // Check if expected and actual results are equal
+        QueryResultAssert.assertEquals(expectedResult, (AssignmentsQueryResult) pageData, true, true);
+    }
+
+}
