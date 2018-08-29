98e48bd2888c2d461d7b96efee9d55585b54edfc
Unknown <roman.margolis@gmail.com>
2018-08-29 13:37:20 +0300

    added more tests for quant1 prop grouping

--- fuse-asg/src/test/java/com/kayhut/fuse/asg/strategy/propertyGrouping/Quant1PropertiesGroupingAsgStrategyTest.java
+++ fuse-asg/src/test/java/com/kayhut/fuse/asg/strategy/propertyGrouping/Quant1PropertiesGroupingAsgStrategyTest.java
@@ -133,7 +133,7 @@ public class Quant1PropertiesGroupingAsgStrategyTest {
     }
 
     @Test
-    public void test_2_nested_quant_with_1_eProps() {
+    public void test_2_nested_quants_with_1_eProps() {
         AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
                     .in(quant1(3, all).next(eProp(4, "p1", of(eq, "abc"))),
                         quant1(5, all).next(eProp(6, "p2", of(eq, 1))))
@@ -151,7 +151,7 @@ public class Quant1PropertiesGroupingAsgStrategyTest {
     }
 
     @Test
-    public void test_2_nested_quant_with_2_eProps() {
+    public void test_2_nested_quants_with_2_eProps() {
         AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
                 .in(quant1(3, all)
                                 .next(eProp(4, "p1", of(eq, "abc")))
@@ -177,7 +177,7 @@ public class Quant1PropertiesGroupingAsgStrategyTest {
     }
 
     @Test
-    public void test_2_nested_quant_with_3_eProps() {
+    public void test_2_nested_quants_with_3_eProps() {
         AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
                 .in(quant1(3, all)
                                 .next(eProp(4, "p1", of(eq, "abc")))
@@ -198,6 +198,154 @@ public class Quant1PropertiesGroupingAsgStrategyTest {
                                 EProp.of(5, "p2", of(eq, 1)),
                                 EProp.of(6, "p3", of(gt, 6))),
                         EPropGroup.of(8,
+                                EProp.of(8, "p4", of(inSet, Arrays.asList("s", "t"))),
+                                EProp.of(9, "p5", of(like, "*def*")),
+                                EProp.of(10, "p6", of(likeAny, Arrays.asList("s*", "*t"))))))
+                .build();
+
+        Assert.assertEquals(expectedQuery, query);
+    }
+
+    @Test
+    public void test_2_nested_all_some_quants_with_1_eProps() {
+        AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
+                .in(quant1(3, some).next(eProp(4, "p1", of(eq, "abc"))),
+                        quant1(5, some).next(eProp(6, "p2", of(eq, 1))))
+                .build();
+
+        new Quant1PropertiesGroupingAsgStrategy().apply(query, new AsgStrategyContext(null));
+
+        AsgQuery expectedQuery = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
+                .in(ePropGroup(4,
+                        EPropGroup.of(4, some, EProp.of(4, "p1", of(eq, "abc"))),
+                        EPropGroup.of(6, some, EProp.of(6, "p2", of(eq, 1)))))
+                .build();
+
+        Assert.assertEquals(expectedQuery, query);
+    }
+
+    @Test
+    public void test_2_nested_some_all_quants_with_1_eProps() {
+        AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, some))
+                .in(quant1(3, all).next(eProp(4, "p1", of(eq, "abc"))),
+                        quant1(5, all).next(eProp(6, "p2", of(eq, 1))))
+                .build();
+
+        new Quant1PropertiesGroupingAsgStrategy().apply(query, new AsgStrategyContext(null));
+
+        AsgQuery expectedQuery = start("q1", "ont").next(typed(1, "1")).next(quant1(2, some))
+                .in(ePropGroup(4, some,
+                        EPropGroup.of(4, all, EProp.of(4, "p1", of(eq, "abc"))),
+                        EPropGroup.of(6, all, EProp.of(6, "p2", of(eq, 1)))))
+                .build();
+
+        Assert.assertEquals(expectedQuery, query);
+    }
+
+    @Test
+    public void test_2_nested_all_some_quants_with_2_eProps() {
+        AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
+                .in(quant1(3, some)
+                                .next(eProp(4, "p1", of(eq, "abc")))
+                                .next(eProp(5, "p2", of(eq, 1))),
+                        quant1(6, some)
+                                .next(eProp(7, "p3", of(gt, 6)))
+                                .next(eProp(8, "p4", of(inSet, Arrays.asList("s", "t")))))
+                .build();
+
+        new Quant1PropertiesGroupingAsgStrategy().apply(query, new AsgStrategyContext(null));
+
+        AsgQuery expectedQuery = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
+                .in(ePropGroup(4,
+                        EPropGroup.of(4, some,
+                                EProp.of(4, "p1", of(eq, "abc")),
+                                EProp.of(5, "p2", of(eq, 1))),
+                        EPropGroup.of(7, some,
+                                EProp.of(7, "p3", of(gt, 6)),
+                                EProp.of(8, "p4", of(inSet, Arrays.asList("s", "t"))))))
+                .build();
+
+        Assert.assertEquals(expectedQuery, query);
+    }
+
+    @Test
+    public void test_2_nested_some_all_quants_with_2_eProps() {
+        AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, some))
+                .in(quant1(3, all)
+                                .next(eProp(4, "p1", of(eq, "abc")))
+                                .next(eProp(5, "p2", of(eq, 1))),
+                        quant1(6, all)
+                                .next(eProp(7, "p3", of(gt, 6)))
+                                .next(eProp(8, "p4", of(inSet, Arrays.asList("s", "t")))))
+                .build();
+
+        new Quant1PropertiesGroupingAsgStrategy().apply(query, new AsgStrategyContext(null));
+
+        AsgQuery expectedQuery = start("q1", "ont").next(typed(1, "1")).next(quant1(2, some))
+                .in(ePropGroup(4, some,
+                        EPropGroup.of(4, all,
+                                EProp.of(4, "p1", of(eq, "abc")),
+                                EProp.of(5, "p2", of(eq, 1))),
+                        EPropGroup.of(7, all,
+                                EProp.of(7, "p3", of(gt, 6)),
+                                EProp.of(8, "p4", of(inSet, Arrays.asList("s", "t"))))))
+                .build();
+
+        Assert.assertEquals(expectedQuery, query);
+    }
+
+    @Test
+    public void test_2_nested_all_some_quants_with_3_eProps() {
+        AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
+                .in(quant1(3, some)
+                                .next(eProp(4, "p1", of(eq, "abc")))
+                                .next(eProp(5, "p2", of(eq, 1)))
+                                .next(eProp(6, "p3", of(gt, 6))),
+                        quant1(7, some)
+                                .next(eProp(8, "p4", of(inSet, Arrays.asList("s", "t"))))
+                                .next(eProp(9, "p5", of(like, "*def*")))
+                                .next(eProp(10, "p6", of(likeAny, Arrays.asList("s*", "*t")))))
+                .build();
+
+        new Quant1PropertiesGroupingAsgStrategy().apply(query, new AsgStrategyContext(null));
+
+        AsgQuery expectedQuery = start("q1", "ont").next(typed(1, "1")).next(quant1(2, all))
+                .in(ePropGroup(4,
+                        EPropGroup.of(4, some,
+                                EProp.of(4, "p1", of(eq, "abc")),
+                                EProp.of(5, "p2", of(eq, 1)),
+                                EProp.of(6, "p3", of(gt, 6))),
+                        EPropGroup.of(8, some,
+                                EProp.of(8, "p4", of(inSet, Arrays.asList("s", "t"))),
+                                EProp.of(9, "p5", of(like, "*def*")),
+                                EProp.of(10, "p6", of(likeAny, Arrays.asList("s*", "*t"))))))
+                .build();
+
+        Assert.assertEquals(expectedQuery, query);
+    }
+
+    @Test
+    public void test_2_nested_some_all_quants_with_3_eProps() {
+        AsgQuery query = start("q1", "ont").next(typed(1, "1")).next(quant1(2, some))
+                .in(quant1(3, all)
+                                .next(eProp(4, "p1", of(eq, "abc")))
+                                .next(eProp(5, "p2", of(eq, 1)))
+                                .next(eProp(6, "p3", of(gt, 6))),
+                        quant1(7, all)
+                                .next(eProp(8, "p4", of(inSet, Arrays.asList("s", "t"))))
+                                .next(eProp(9, "p5", of(like, "*def*")))
+                                .next(eProp(10, "p6", of(likeAny, Arrays.asList("s*", "*t")))))
+                .build();
+
+        new Quant1PropertiesGroupingAsgStrategy().apply(query, new AsgStrategyContext(null));
+
+        AsgQuery expectedQuery = start("q1", "ont").next(typed(1, "1")).next(quant1(2, some))
+                .in(ePropGroup(4, some,
+                        EPropGroup.of(4, all,
+                                EProp.of(4, "p1", of(eq, "abc")),
+                                EProp.of(5, "p2", of(eq, 1)),
+                                EProp.of(6, "p3", of(gt, 6))),
+                        EPropGroup.of(8, all,
                                 EProp.of(8, "p4", of(inSet, Arrays.asList("s", "t"))),
                                 EProp.of(9, "p5", of(like, "*def*")),
                                 EProp.of(10, "p6", of(likeAny, Arrays.asList("s*", "*t"))))))
