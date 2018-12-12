c5189ac43ed06b48abecefc50d2d9bbba7fbe570
lior perry <lior.perry@kayhut.com>
2018-12-12 11:30:41 +0200

    add bool algebra support for Converting to Disjunctive Normal (Sum-of-Products) of the where clause * fix printing of below rel group * fix clone(enum) of EBase entities * add tests for OR expression chains

--- fuse-asg/src/test/java/com/kayhut/fuse/asg/translator/cypher/CypherMatchWithWhereOrOpLabelTranslatorTest.java
+++ fuse-asg/src/test/java/com/kayhut/fuse/asg/translator/cypher/CypherMatchWithWhereOrOpLabelTranslatorTest.java
@@ -94,8 +94,9 @@ public class CypherMatchWithWhereOrOpLabelTranslatorTest {
                                                                     .next(quant1(700, all)
                                                                             .addNext(ePropGroup(701, all,
                                                                                     of(701, "type",
-                                                                                            of(inSet, Arrays.asList("Person")))))))
+                                                                                            of(inSet, Arrays.asList("Person"))))))
                                                     )
+                                                )
                                         ),
                         unTyped(8, "a")
                                 .addNext(
@@ -151,6 +152,142 @@ public class CypherMatchWithWhereOrOpLabelTranslatorTest {
                                                                         unTyped(11, "b"))
                                                 )
                                                 .addNext(ePropGroup(801, all,
+                                                        of(801, "type",
+                                                                of(inSet, Arrays.asList("Dragon"))),
+                                                        of(802, "type",
+                                                                of(inSet, Arrays.asList("Hours"))))
+                                                )
+                                )
+                ).build();
+        assertEquals(print(expected), print(query));
+    }
+
+    @Test
+    public void testMatch_A_AND_where_A_OfType_And_OR_B_OfType_Return_() {
+        AsgTranslator<String, AsgQuery> translator = new CypherTranslator("Dragons", Collections.singleton(match));
+        final AsgQuery query = translator.translate("MATCH (a)-[c]-(b) where (a:Dragon Or c:Fire) And b:Person RETURN a");
+
+        AsgQuery expected = AsgQuery.Builder
+                .start("cypher_", "Dragons")
+                .next(quant1(300, some))
+                .in(
+                        unTyped(4, "a")
+                                .addNext(
+                                        quant1(400, all)
+                                                .addNext(
+                                                        rel(6,null,Rel.Direction.RL,"c")
+                                                                .below(relPropGroup(600,all,
+                                                                        RelProp.of(60100, "type", of(inSet, Arrays.asList("Fire")))))
+                                                                .next(
+                                                                        unTyped(7, "b")
+                                                                                .next(quant1(700, all)
+                                                                                        .addNext(ePropGroup(701, all,
+                                                                                                of(701, "type",
+                                                                                                        of(inSet, Arrays.asList("Person"))))))
+                                                                )
+                                                )
+                                ),
+                        unTyped(8, "a")
+                                .addNext(
+                                        quant1(800, all)
+                                                .addNext(
+                                                        rel(10,null,Rel.Direction.RL,"c")
+                                                                .next(
+                                                                        unTyped(11, "b")
+                                                                                .next(quant1(70100, all)
+                                                                                        .addNext(ePropGroup(70101, all,
+                                                                                                of(70101, "type",
+                                                                                                        of(inSet, Arrays.asList("Person"))))))
+                                                                )
+                                                )
+                                                .addNext(ePropGroup(801, all,
+                                                        of(801, "type",
+                                                                of(inSet, Arrays.asList("Dragon"))))
+                                                )
+                                )
+                ).build();
+        assertEquals(print(expected), print(query));
+    }
+    @Test
+    public void testMatch_A_AND_where_A_OfType_And_OR_B_OfType_AND_b_Return_() {
+        AsgTranslator<String, AsgQuery> translator = new CypherTranslator("Dragons", Collections.singleton(match));
+        final AsgQuery query = translator.translate("MATCH (a)-[c]-(b) where ((a:Dragon And a:Hours) Or c:Fire) And b:Person RETURN a");
+
+        AsgQuery expected = AsgQuery.Builder
+                .start("cypher_", "Dragons")
+                .next(quant1(300, some))
+                .in(
+                        unTyped(4, "a")
+                                .addNext(
+                                        quant1(400, all)
+                                                .addNext(
+                                                        rel(6,null,Rel.Direction.RL,"c")
+                                                                .next(
+                                                                        unTyped(7, "b")
+                                                                                .next(quant1(700, all)
+                                                                                        .addNext(ePropGroup(701, all,
+                                                                                                of(701, "type",
+                                                                                                        of(inSet, Arrays.asList("Person"))))))
+                                                                )
+                                                )
+                                                .addNext(ePropGroup(401, all,
+                                                        of(401, "type",
+                                                                of(inSet, Arrays.asList("Dragon"))),
+                                                        of(402, "type",
+                                                                of(inSet, Arrays.asList("Hours")))
+                                                        )
+                                                )
+                                ),
+                        unTyped(8, "a")
+                                .addNext(
+                                        quant1(800, all)
+                                                .addNext(
+                                                        rel(10,null,Rel.Direction.RL,"c")
+                                                                .below(relPropGroup(1000,all,
+                                                                        RelProp.of(100100, "type", of(inSet, Arrays.asList("Fire")))))
+                                                                .next(
+                                                                        unTyped(11, "b")
+                                                                                .next(quant1(70100, all)
+                                                                                        .addNext(ePropGroup(70101, all,
+                                                                                                of(70101, "type",
+                                                                                                        of(inSet, Arrays.asList("Person"))))))
+                                                                )
+                                                )
+                                )
+                ).build();
+        assertEquals(print(expected), print(query));
+    }
+
+    @Test
+    @Ignore
+    //todo add Rule for same variable with multiple operators
+    public void testMatch_A_OR_where_A_OfType_And_OR_B_OfType_AND_Return_() {
+        AsgTranslator<String, AsgQuery> translator = new CypherTranslator("Dragons", Collections.singleton(match));
+        final AsgQuery query = translator.translate("MATCH (a)-[c]-(b) where (a:Dragon OR a:Hours) Or c:Fire RETURN a");
+
+        AsgQuery expected = AsgQuery.Builder
+                .start("cypher_", "Dragons")
+                .next(quant1(300, some))
+                .in(
+                        unTyped(4, "a")
+                                .addNext(
+                                        quant1(400, all)
+                                                .addNext(
+                                                        rel(6,null,Rel.Direction.RL,"c")
+                                                                .below(relPropGroup(600,all,
+                                                                        RelProp.of(60100, "type", of(inSet, Arrays.asList("Fire")))))
+                                                                .next(
+                                                                        unTyped(7, "b")))
+                                ),
+                        unTyped(8, "a")
+                                .addNext(
+                                        quant1(800, all)
+                                                .addNext(
+                                                        rel(10,null,Rel.Direction.RL,"c")
+                                                                .next(
+                                                                        unTyped(11, "b"))
+                                                )
+                                                .addNext(ePropGroup(801, some,
                                                         of(801, "type",
                                                                 of(inSet, Arrays.asList("Dragon"))),
                                                         of(802, "type",
