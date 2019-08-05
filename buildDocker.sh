601ef92760992a8278e848c32b2791094b18619c
liordana@gmail.com <liordana@gmail.com>
2019-08-05 15:16:40 +0300

    docker build & push script

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,10 +2,10 @@
 
 tagname=$1
 
-echo "docker build -f Dockerfile -t tom4tomato/yang.db:${tagname}"
+echo "docker build . -t tom4tomato/yang.db:${tagname}"
 cd $(dirname "$0")
 
-docker build -f Dockerfile -t tom4tomato/yang.db:${tagname}
+docker build . -t tom4tomato/yang.db:${tagname}
 
 echo "docker push tom4tomato/yang.db:${tagname}"
 docker push tom4tomato/yang.db:${tagname}
