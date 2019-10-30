ae2ca3800b4f30a1bb88766c970f5aeb54ba3957
tom4tomato@gmail.com <tom4tomato@gmail.com>
2019-10-30 23:52:23 +0200

    add tar artifact

--- buildDocker.sh
+++ buildDocker.sh
@@ -8,10 +8,10 @@ docker login -u yangdb
 
 cd $(dirname "$0")
 echo "mvn clean install"
-mvn clean install
+#mvn clean install
 
 echo "docker build . -t yangdb/yang.db:${tagname}"
-docker build . -t yangdb/yang.db:${tagname}
+docker build -f Dockerfile -t yangdb/yang.db:${tagname}
 
 echo "docker push yangdb/yang.db:${tagname}"
 docker push yangdb/yang.db:${tagname}
