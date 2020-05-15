610c32805c71f35cf8fac0123db4260379c59b51
yang-db <yang.db.dev@gmail.com>
2020-05-16 00:11:48 +0300

    Add profiling information on each traversal step - count hits vs elastic controller Add tagging for each step via ASG strategy (all steps must have tags)

--- fuse-dv/fuse-dv-core/src/main/java/com/yangdb/fuse/executor/cursor/discrete/ForwardOnlyPathsTraversalCursor.java
+++ fuse-dv/fuse-dv-core/src/main/java/com/yangdb/fuse/executor/cursor/discrete/ForwardOnlyPathsTraversalCursor.java
@@ -9,9 +9,9 @@ package com.yangdb.fuse.executor.cursor.discrete;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -60,31 +60,13 @@ public class ForwardOnlyPathsTraversalCursor extends PathsTraversalCursor {
     //region Private Methods
     protected AssignmentsQueryResult toQuery(int numResults) {
         AssignmentsQueryResult.Builder builder = instance();
-        AsgQuery asgQuery = getContext().getQueryResource().getAsgQuery();
-        //check is Or quant exists -> if so take any largest path
-        if (AsgQueryUtil.element(asgQuery, Quant1.class).isPresent()
-                && AsgQueryUtil.element(asgQuery, Quant1.class).get().geteBase().getqType().equals(QuantType.some)) {
-            //in case of Union -> return all sub-paths regardless of max elements in path
-            (getContext().getTraversal().next(numResults)).forEach(path -> {
-                //todo make sure no circle exists in path
-                if(path.objects().stream().map(p-> ((UniElement)p).id().toString()).collect(Collectors.toSet()).size() == path.objects().size())
-                    builder.withAssignment(toAssignment(path));
-            });
-        } else {
-            int nodes = AsgQueryUtil.elements(asgQuery, EEntityBase.class).size();
-            int edges = AsgQueryUtil.elements(asgQuery, Rel.class).size();
-            final Query pattern = getContext().getQueryResource().getQuery();
-            builder.withPattern(pattern);
-            //build assignments
-            (getContext().getTraversal().next(numResults)).forEach(path -> {
-                Assignment assignments = toAssignment(path);
-                if (assignments.getEntities().size() == nodes && assignments.getRelationships().size() == edges) {
-                    //todo make sure no circle exists in path
-                    if(path.objects().stream().map(p-> ((UniElement)p).id().toString()).collect(Collectors.toSet()).size() == path.objects().size())
-                        builder.withAssignment(assignments);
-                }
-            });
-        }
+        final Query pattern = getContext().getQueryResource().getQuery();
+        builder.withPattern(pattern);
+        (getContext().getTraversal().next(numResults)).forEach(path -> {
+            //makes sure no circle exists in path (no getting back - forward only assignments)
+            if (path.objects().stream().map(p -> ((UniElement) p).id().toString()).collect(Collectors.toSet()).size() == path.objects().size())
+                builder.withAssignment(toAssignment(path));
+        });
         return builder.build();
     }
 
