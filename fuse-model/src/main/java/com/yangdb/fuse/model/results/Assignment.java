29f24b9a98b69eea8f49d62776d0885f0786cc53
YANG-DB <yang.db.dev@gmail.com>
2022-03-27 18:35:33 +0300

    fix dependencies

--- fuse-model/src/main/java/com/yangdb/fuse/model/results/Assignment.java
+++ fuse-model/src/main/java/com/yangdb/fuse/model/results/Assignment.java
@@ -20,7 +20,7 @@ package com.yangdb.fuse.model.results;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * #L%
  */
 
 
