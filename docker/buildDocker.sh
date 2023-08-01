db0a295e9cc517fbd36492913a491098f17210fd
YANGDB <yang.db.dev@gmail.com>
2023-07-31 19:41:47 -0700

    Update docker-compose.yml with the OpenSearch images and Dockerfile with jdk 11
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- docker/buildDocker.sh
+++ docker/buildDocker.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+# Set the tag name or take the current date as the default tag
+if [ -z "$1" ]; then
+  tagname=$(date +'%Y%m%d')
+else
+  tagname=$1
+fi
 
-tagname=$1
-
+# Login to docker using the yandb Account
 echo "docker login -u yangdb"
 docker login -u yangdb
 
-cd $(dirname "$0")
-echo "mvn clean install"
-mvn clean install
+# build the project for the dragons assembly distrib folder
+cd ..
+echo "mvn clean install -DskipTests=true install -P core,dragons-assembly"
+#mvn clean install -DskipTests=true install -P core,dragons-assembly
 
+# Copy Dockerfile to the distrib folder
+cd docker
+cp Dockerfile ../distrib/
+
+# Build the Docker image in the distrib folder
+cd ..
+cd distrib
 echo "docker build -t yangdb/yang.db:${tagname} ."
 docker build -t yangdb/yang.db:${tagname} .
 
+# push the docker image to the docker hub
 echo "docker push yangdb/yang.db:${tagname}"
 docker push yangdb/yang.db:${tagname}
 
